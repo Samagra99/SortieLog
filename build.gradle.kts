dc9d7cc8b0b3d652da4ab3655fea96e7021c1123
Samagra99 <68296421+Samagra99@users.noreply.github.com>
2025-08-11 21:01:00 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,5 @@
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google(); mavenCentral()
-    }
+plugins {
+    // These are applied to submodules, not the root itself
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
-rootProject.name = "SortieLoggerFDTL"
-include(":app")
